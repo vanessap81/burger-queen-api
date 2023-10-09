3e47f2f8da7da63bc5a8ade6dd9f2771a8cafd1a
Gabriela Faria <gabrielafaria806@gmail.com>
2023-10-09 10:17:12 -0300

    T001: initial setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const config = require('./config');
 const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
@@ -8,7 +9,13 @@ const pkg = require('./package.json');
 const { port, dbUrl, secret } = config;
 const app = express();
 
-// TODO: Conexión a la Base de Datos (MongoDB o MySQL)
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log)
+  .catch(console.error);
 
 app.set('config', config);
 app.set('pkg', pkg);
